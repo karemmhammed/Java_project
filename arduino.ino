b363c6d117cbcf8c064c493babe26bf20660956e
Ahmed Hassan <ahmedhassanhosny5@gmail.com>
2021-12-30 21:31:36 +0200

    Implementing Arduino Code for Connection to Java

--- arduino.ino
+++ arduino.ino
@@ -27,4 +27,27 @@ void MotorStop(){
 } 
 
 void loop()
-{}
+{
+	
+	if(Serial.available()){
+    x= Serial.read();
+    
+   switch(x){
+    case 255: MotorDirection=1;break;
+    case 254: MotorDirection=0;break;
+    case 253: MotorState=0;break;
+    case 252: MotorState=1;break;
+    default:  PWM = x;
+   }
+
+   if(MotorDirection == 1 && MotorState==1){
+          MotorClkWise();
+   }
+   else if(MotorDirection == 0 && MotorState==1){
+          MotorAntiClkWise();
+   }
+   else if (MotorState==0){
+          MotorStop();
+   }
+	
+}
